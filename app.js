1d744dc4310574e6307861ad9c934ccd0cb267a4
SENES <caro67200@gmail.com>
2021-08-07 21:24:10 +0200

    Nav animate html+css+js

---
+++ app.js
@@ -0,0 +1,10 @@
+//Nav Animation
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > 30){
+        nav.classList.add('nav-animate')
+    }else{
+        nav.classList.remove('nav-animate');
+    }
+})
